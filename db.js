83e938055f3cf79127eb3d034ed7571eb1668636
S-Dharshini05 <sugaadharshini@gmail.com>
2026-06-19 01:34:39 +0530

    fix TiDB connection pool

--- db.js
+++ db.js
@@ -1,24 +1,32 @@
 const mysql = require("mysql2");
 require("dotenv").config();
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+
   ssl: {
     rejectUnauthorized: true
-  }
+  },
+
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-// DO NOT BLOCK SERVER START (IMPORTANT)
-db.connect((err) => {
+// Test connection
+db.getConnection((err, connection) => {
   if (err) {
     console.error("DB connection failed:", err.message);
     return;
   }
+
   console.log("Connected to TiDB successfully!");
+
+  connection.release();
 });
 
 module.exports = db;
